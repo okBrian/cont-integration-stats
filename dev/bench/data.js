edd927854b6926147052ced983bfe2d92b3c9e30
github-action-benchmark <github@users.noreply.github.com>
2024-11-06 19:28:36 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 14b0eb68143cf48fea080d25279bed34f1be1a33

---
+++ dev/bench/data.js
@@ -0,0 +1,37 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1730921316230,
+  "repoUrl": "https://github.com/okBrian/cont-integration-test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "brianok924@gmail.com",
+            "name": "Brian Ok",
+            "username": "okBrian"
+          },
+          "committer": {
+            "email": "brianok924@gmail.com",
+            "name": "Brian Ok",
+            "username": "okBrian"
+          },
+          "distinct": true,
+          "id": "14b0eb68143cf48fea080d25279bed34f1be1a33",
+          "message": "asd",
+          "timestamp": "2024-11-06T14:28:22-05:00",
+          "tree_id": "0a3ce3869dcf7c8fa0f15e3cddae055981aea131",
+          "url": "https://github.com/okBrian/cont-integration-test/commit/14b0eb68143cf48fea080d25279bed34f1be1a33"
+        },
+        "date": 1730921315957,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 60,
+            "unit": "Percent"
+          }
+        ]
+      }
+    ]
+  }
+}
